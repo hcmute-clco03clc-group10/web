a6285dd711671f632ff0cb9a3af40c762b6c03a7
Knguyenhanh <you@example.com>
2022-11-01 18:20:50 +0700

    Update button.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -33,7 +33,7 @@
                         </div>
 
                         <div>
-                            <button type="submit" class="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Log in</button>
+                            <button type="submit" class="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Login</button>
                         </div>
                     </form>
                     <div class="relative my-4">
