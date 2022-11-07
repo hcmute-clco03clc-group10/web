a7721d27878129f8e1dfc810908c09afc4e347ae
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-07 14:26:58 +0700

    Change background color to slate-50.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
 	});
 </script>
 
-<div class="max-w-screen min-h-screen py-4 px-2 flex flex-col justify-start gap-y-10">
+<div class="max-w-screen min-h-screen py-4 px-2 flex flex-col justify-start gap-y-10 bg-slate-50">
 	<header>
 		<div class="mx-auto flex max-w-7xl items-center justify-between p-4">
 			<div class="flex items-center space-x-2">
@@ -28,16 +28,16 @@
 				</a>
 			</div>
 			<nav class="hidden items-center space-x-2 text-sm font-medium text-gray-800 md:flex">
-				<a href="/about" class="rounded bg-white px-3 py-2 transition hover:bg-gray-100">About</a>
+				<a href="/about" class="rounded bg-slate-50 px-3 py-2 transition hover:bg-gray-100">About</a>
 			</nav>
 			<nav class="flex items-center space-x-1 text-sm font-medium text-gray-800">
 				<a
 					href="https://github.com/hcmute-clco03clc-group11"
-					class="hidden rounded bg-white px-3 py-2 transition hover:bg-gray-100 sm:inline">Login</a
+					class="hidden rounded bg-slate-50 px-3 py-2 transition hover:bg-gray-100 sm:inline">Login</a
 				>
 				<a
 					href="https://github.com/hcmute-clco03clc-group11"
-					class="rounded bg-blue-600 px-3 py-2 text-white transition hover:bg-blue-700">Sign Up</a
+					class="rounded bg-blue-600 px-3 py-2 text-slate-50 transition hover:bg-blue-700">Sign Up</a
 				>
 			</nav>
 		</div>
