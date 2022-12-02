6d5994f1203d4eefb4fd2615e1fddbb75d34e9f3
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-02 18:34:35 +0700

    Add Input component.

---
+++ src/lib/Input.svelte
@@ -0,0 +1,5 @@
+<input
+	on:input
+	{...$$props}
+	class="rounded px-5 py-3 text-base text-slate-600 placeholder-slate-400 transition duration-200 ease-in-out transform border-slate-400 bg-slate-50 focus:outline-none focus:border-transparent focus:ring focus:ring-blue-600/60 {$$props.class}"
+/>
