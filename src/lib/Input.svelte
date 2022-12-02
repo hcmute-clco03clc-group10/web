577a46aeadf4661e12eb040e43fbe39b3fb0d917
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-03 02:23:53 +0700

    Add value props.

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	export let value: string | undefined;
+</script>
+
 <input
 	on:input
-	{...$$props}
-	class="rounded px-5 py-3 text-base text-slate-600 placeholder-slate-400 transition duration-200 ease-in-out transform border-slate-400 bg-slate-50 focus:outline-none focus:border-transparent focus:ring focus:ring-blue-600/60 {$$props.class}"
+	bind:value
+	{...$$restProps}
+	class="rounded px-5 py-3 text-base text-slate-600 placeholder-slate-400 transition duration-200 ease-in-out transform border-slate-400 bg-slate-50 focus:outline-none focus:border-transparent focus:ring focus:ring-blue-600/60 {$$restProps.class}"
 />
