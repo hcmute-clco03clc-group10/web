18c47484472139c6dbbfac9c3356572c145e02eb
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-03 12:05:31 +0700

    Add Link component.

---
+++ src/lib/Link.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let href: string;
+</script>
+<a
+	href={href}
+	{...$$restProps}
+	class="relative font-medium text-blue-600 after:absolute after:bg-blue-600 after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 {$$restProps.class}"
+>
+	<slot />
+</a>
