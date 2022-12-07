8280cf156f21333b465c6e24b12a2de26238ae67
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-07 16:12:08 +0700

    Add SymbolLink component.

---
+++ src/lib/SymbolLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let solid = false;
+	export let hover = false;
+	export let href: string;
+</script>
+
+<a
+	{...$$restProps}
+	href={href}
+	class={'group relative flex items-center overflow-hidden rounded transition transform duration-200 ease-in-out focus:outline-none focus:ring focus:ring-blue-600/60 ' +
+		(solid ? 'bg-blue-600 text-slate-100 border-blue-700' : 'border-blue-600 text-blue-600 ') +
+		($$restProps.class || '')}
+>
+	<span
+	 class="absolute right-0 translate-x-full flex items-center transition-transform {hover ? '-translate-x-0' : 'group-hover:-translate-x-0'}"
+	>
+		<slot name="symbol" />
+	</span>
+	<span class="font-medium transition-all {hover ? 'mr-6' : 'group-hover:mr-6'}">
+		<slot name="text" />
+	</span>
+</a>
