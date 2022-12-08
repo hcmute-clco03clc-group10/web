e8ee79d287f83e5791b45b94336acc17a31cdafa
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-09 00:35:27 +0700

    Add side layout slot transition.

---
+++ src/routes/(side)/(logged)/dashboard/user/+page.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+	<title>Cloudbase • Dashboard • User</title>
+</svelte:head>
+User dashboard
--- src/routes/(side)/+layout.svelte
+++ src/routes/(side)/+layout.svelte
@@ -3,10 +3,15 @@
 	import { beforeNavigate, afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import SideBar from '$lib/SideBar.svelte';
+	import SkeletonBar from '$lib/SkeletonBar.svelte';
+	import { fade, fly } from 'svelte/transition';
+	import Skeleton from '$lib/Skeleton.svelte';
+	import { cubicIn, cubicOut } from 'svelte/easing';
 
 	let navigating = false;
 	let promise: Promise<void> | undefined;
 	beforeNavigate((nav) => {
+		console.log(nav);
 		if (promise) {
 			nav.cancel();
 			return;
@@ -15,11 +20,11 @@
 			setTimeout(() => {
 				resolve();
 				promise = undefined;
-			}, 3500)
+			}, 500)
 		);
 		navigating = true;
 	});
-	afterNavigate(async () => {
+	afterNavigate(async (nav) => {
 		await promise;
 		navigating = false;
 	});
@@ -35,11 +40,42 @@
 				}
 			]
 		},
-		{ text: 'User', symbol: 'person' }
+		{ text: 'User', symbol: 'person', href: '/dashboard/user' }
 	];
 </script>
 
 <section class="flex justify-start items-start">
 	<SideBar {items} user={$page.data.userRef.deref()} />
-	<slot />
+	<main class="relative p-6 w-full min-h-screen">
+		{#await promise}
+			<div
+				class="absolute t-0 l-0 w-3/4 flex flex-col gap-y-6"
+				in:fade={{ delay: 100, duration: 200, easing: cubicOut }}
+				out:fly={{ x: 10, duration: 100, easing: cubicIn }}
+			>
+				<SkeletonBar via="via-slate-300" class="w-1/2 h-12" />
+				<SkeletonBar via="via-slate-300" class="w-3/4 h-6" />
+				<SkeletonBar via="via-slate-300" class="w-1/3 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-1/4 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-1/5 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-1/4 h-12" />
+				<SkeletonBar via="via-slate-300" class="w-2/5 h-8" />
+				<SkeletonBar via="via-slate-300" class="w-1/2 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-1/5 h-10" />
+				<SkeletonBar via="via-slate-300" class="w-1/4 h-6" />
+				<SkeletonBar via="via-slate-300" class="w-3/4 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-2/3 h-8" />
+				<SkeletonBar via="via-slate-300" class="w-1/3 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-1/4 h-4" />
+				<SkeletonBar via="via-slate-300" class="w-2/5 h-10" />
+			</div>
+		{:then}
+			<section
+				in:fly={{ x: -10, delay: 100, duration: 200, easing: cubicOut }}
+				out:fade={{ duration: 100, easing: cubicIn }}
+			>
+				<slot />
+			</section>
+		{/await}
+	</main>
 </section>
