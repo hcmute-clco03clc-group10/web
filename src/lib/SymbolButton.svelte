91c972c24a2f1747d185c8ff0ace5b1638fc3d56
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-08 23:17:44 +0700

    Adjust symbol link translate.

--- src/lib/SymbolButton.svelte
+++ src/lib/SymbolButton.svelte
@@ -6,11 +6,13 @@
 <button
 	{...$$restProps}
 	class={'group relative flex items-center overflow-hidden rounded px-8 py-3 transition transform duration-200 ease-in-out focus:outline-none focus:border-transparent border focus:ring focus:ring-blue-600/60 ' +
-		(solid ? 'bg-blue-600 text-slate-100 border-blue-700' : 'border-blue-600 text-blue-600 ') +
+		(solid ? 'bg-blue-600 text-slate-100 border-blue-700 ' : 'border-blue-600 text-blue-600 ') +
 		($$restProps.class || '')}
 >
 	<span
-	 class="absolute right-0 translate-x-full flex items-center transition-transform {hover ? '-translate-x-4' : 'group-hover:-translate-x-4'}"
+		class="absolute right-0 translate-x-full flex items-center transition-transform {hover
+			? '-translate-x-4'
+			: 'group-hover:-translate-x-4'}"
 	>
 		<slot name="symbol" />
 	</span>
