1ca22f3c81d197d6b79388db7dd2275cef46772c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-08 17:00:26 +0700

    Correct type import.

--- src/routes/(topdown)/+layout.ts
+++ src/routes/(topdown)/+layout.ts
@@ -1,10 +1,10 @@
 import { endpoint } from '$lib/api';
-import type { PageLoad } from './$types';
+import type { LayoutLoad } from './$types';
 
 export const prerender = true;
 export const ssr = true;
 
-export const load: PageLoad = ({ fetch }) => {
+export const load: LayoutLoad = ({ fetch }) => {
 	return {
 		logging: new WeakRef(
 			fetch(endpoint('/token'), { method: 'get', credentials: 'include' }).then(
