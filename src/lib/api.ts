1efc399faaa2daa5cec16ab34cd4076798bd1d4c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-08 23:27:37 +0700

    Format code.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,4 +2,4 @@ const apiAddress = 'https://w90wttueb3.execute-api.us-east-1.amazonaws.com/v1';
 
 export const endpoint = (path: string) => {
 	return apiAddress + path;
-}
+};
--- src/routes/(side)/(logged)/+layout.ts
+++ src/routes/(side)/(logged)/+layout.ts
@@ -4,13 +4,11 @@ import type { LayoutLoad } from './$types';
 
 export const ssr = false;
 
-export const load: LayoutLoad = async (ev) => {
-	const logged = await ev
-		.fetch(endpoint('/token'), {
-			method: 'get',
-			credentials: 'include'
-		})
-		.then(({ status }) => status === 200);
+export const load: LayoutLoad = async ({ fetch }) => {
+	const logged = await fetch(endpoint('/token'), {
+		method: 'get',
+		credentials: 'include'
+	}).then(({ status }) => status === 200);
 	if (!logged) {
 		throw error(403, 'not logged in');
 	}
