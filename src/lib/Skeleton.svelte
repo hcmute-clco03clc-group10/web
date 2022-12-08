0351b98300f41fd076711b0edfd805cd237e5965
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-09 00:35:04 +0700

    Use slate color.

--- src/lib/Skeleton.svelte
+++ src/lib/Skeleton.svelte
@@ -1,15 +1,15 @@
 <div role="status" class="max-w-screen animate-pulse">
-	<div class="h-12 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mb-12" />
-	<div class="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-1/2 mt-6 mb-6" />
-	<div class="h-6 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mt-4 mb-6" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4 mb-4" />
-	<div class="h-8 bg-gray-200 rounded-full dark:bg-gray-700 w-1/4 mt-6 mb-6" />
-	<div class="h-6 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mt-4 mb-6" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4 mb-4" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mb-4" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-3/4 mb-4" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-4" />
-	<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mb-4" />
+	<div class="h-12 bg-slate-200 rounded-full dark:bg-slate-700 w-1/3 mb-12" />
+	<div class="h-8 bg-slate-200 rounded-full dark:bg-slate-700 w-1/2 mt-6 mb-6" />
+	<div class="h-6 bg-slate-200 rounded-full dark:bg-slate-700 w-1/3 mt-4 mb-6" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-3/4 mb-4" />
+	<div class="h-8 bg-slate-200 rounded-full dark:bg-slate-700 w-1/4 mt-6 mb-6" />
+	<div class="h-6 bg-slate-200 rounded-full dark:bg-slate-700 w-1/3 mt-4 mb-6" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-3/4 mb-4" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-full mb-4" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-1/3 mb-4" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-3/4 mb-4" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-full mb-4" />
+	<div class="h-3 bg-slate-200 rounded-full dark:bg-slate-700 w-1/3 mb-4" />
 	<span class="sr-only">Loading...</span>
 </div>
