2801e40c096c42044bcc81cc32c8cf753e03e011
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-09 22:21:43 +0700

    Add noPadding prop.

--- src/lib/SymbolButton.svelte
+++ src/lib/SymbolButton.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	export let solid = false;
 	export let hover = false;
+	export let noPadding = false;
 </script>
 
 <button
 	{...$$restProps}
-	class={'group relative flex items-center overflow-hidden rounded px-8 py-3 transition transform duration-200 ease-in-out focus:outline-none focus:border-transparent border focus:ring focus:ring-blue-600/60 ' +
+	class={`group relative flex items-center overflow-hidden rounded ${!noPadding ? 'px-8 py-3' : ''} transition transform duration-200 ease-in-out focus:outline-none focus:border-transparent border focus:ring focus:ring-blue-600/60 ` +
 		(solid ? 'bg-blue-600 text-slate-100 border-blue-700 ' : 'border-blue-600 text-blue-600 ') +
 		($$restProps.class || '')}
 >
