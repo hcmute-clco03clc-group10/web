76751e5d5368d32bd349976b0118d3c9cdd558fd
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-09 22:21:27 +0700

    Update TableList component.

--- src/lib/TableList.svelte
+++ src/lib/TableList.svelte
@@ -1,61 +1,37 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { fly } from 'svelte/transition';
-	import { flip } from 'svelte/animate';
-	import { quadIn, quadOut } from 'svelte/easing';
-	export let header = 'Tables';
-	export let items: string[] = [];
-	onMount(() => {
-		let count = 1;
-		let interval = setInterval(() => {
-			items.push(`Table no. ${count++}`);
-			if (count === 10) {
-				clearInterval(interval);
-			}
-			items = items.sort(() => Math.random() - 0.5);
-		}, 300);
-	});
+	export let items: ITable[] = [];
 </script>
 
-<section class="w-fit text-xl p-3 rounded border border-slate-300">
-	<header class="flex justify-between items-center gap-4">
-		<h2 class="text-2xl font-semibold">{header}</h2>
-		<button
-			type="button"
-			class="material-symbols-rounded align-middle rounded-full transition-color duration-200
-		hover:text-blue-600 hover:ring-1 hover:-ring-offset-2 hover:ring-blue-600"
-		>
-			add_circle
-		</button>
-	</header>
-	<nav class="hidden md:block">
-		<ul class="flex flex-col">
-			{#each items as item (item)}
-				<a
-					class="group mt-3 bg-slate-200 rounded p-2
-					hover:bg-blue-200 transition-color duration-100
-					hover:ring-1 hover:ring-blue-600"
-					animate:flip={{ duration: 500, easing: quadOut }}
-					href="/"
-					in:fly={{ y: -10, duration: 300, easing: quadIn }}
-				>
-					<li>
-						<span
-							class="material-symbols-rounded align-middle text-slate-600 duration-100 transition-color
-							group-hover:text-slate-900"
-						>
-							table_view
-						</span>
-						{item}
-					</li>
-				</a>
-			{/each}
-		</ul>
-	</nav>
-</section>
+<div class="overflow-hidden overflow-x-auto rounded-lg border border-slate-200">
+	<table class="min-w-full divide-y divide-slate-200">
+		<thead class="bg-slate-100">
+			<tr>
+				<th class="bg-slate-100 px-4 py-2 text-left">
+					<label class="sr-only" for="SelectAll">Select All</label>
 
-<style>
-	section {
-		min-width: 6em;
-	}
-</style>
+					<input class="h-5 w-5 rounded border-slate-200" type="checkbox" id="SelectAll" />
+				</th>
+				<th class="whitespace-nowrap px-4 py-2 text-left font-medium text-slate-900"> Name </th>
+				<th class="whitespace-nowrap px-4 py-2 text-left font-medium text-slate-900"> ... </th>
+				<th class="whitespace-nowrap px-4 py-2 text-left font-medium text-slate-900"> ... </th>
+				<th class="whitespace-nowrap px-4 py-2 text-left font-medium text-slate-900"> ... </th>
+			</tr>
+		</thead>
+
+		<tbody class="divide-y divide-slate-200">
+			{#each items as item, i (item)}
+				<tr>
+					<td class="bg-white px-4 py-2">
+						<label class="sr-only" for="Row1">Row {i + 1}</label>
+
+						<input class="h-5 w-5 rounded border-slate-200" type="checkbox" />
+					</td>
+					<td class="whitespace-nowrap px-4 py-2 font-medium"> {item.name} </td>
+					<td class="whitespace-nowrap px-4 py-2 text-slate-700">...</td>
+					<td class="whitespace-nowrap px-4 py-2 text-slate-700">...</td>
+					<td class="whitespace-nowrap px-4 py-2 text-slate-700">...</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
