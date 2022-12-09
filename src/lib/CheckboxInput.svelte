a587d752cfd0242fa9c0ea4395eeeb8529d2549c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-10 05:09:09 +0700

    Add checkbox input component.

---
+++ src/lib/CheckboxInput.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let checked = false;
+</script>
+
+<input
+	type="checkbox"
+	on:input
+	bind:checked
+	{...$$restProps}
+	class="rounded px-5 py-3 text-base text-slate-600 placeholder-slate-400 transition duration-200 ease-in-out transform border-slate-400 bg-slate-50 focus:outline-none focus:border-transparent focus:ring focus:ring-blue-600/60 {$$restProps.class}"
+/>
