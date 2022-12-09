c9ea14efed914aa52a4ed58ffee9346ed1cecd53
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-09 22:20:22 +0700

    Add api.use().

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,30 @@
+type OmittedRequestInit = Omit<RequestInit, 'method' | 'credentials'>;
+
 const apiAddress = 'https://w90wttueb3.execute-api.us-east-1.amazonaws.com/v1';
+
+export const api = {
+	use: function(this: any, fetch: typeof window.fetch) {
+		this.fetch = fetch;
+		return {
+			get: __get.bind(this, fetch),
+			post: __post.bind(this, fetch),
+		}
+	}
+};
+
+const __get = (fetch: typeof window.fetch, path: string, init?: OmittedRequestInit) => {
+	return fetch(endpoint(path), Object.assign({
+		method: 'get',
+		credentials: 'include',
+	}, init) as RequestInit);
+}
+
+const __post = (fetch: typeof window.fetch, path: string, init?: OmittedRequestInit) => {
+	return fetch(endpoint(path), Object.assign({
+		method: 'post',
+		credentials: 'include',
+	}, init) as RequestInit)
+}
 
 export const endpoint = (path: string) => {
 	return apiAddress + path;
