65091a117fa7be84e84f57d568fec991b9fc4908
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-10 05:09:26 +0700

    Decrease bottom.

--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	export let href: string;
 </script>
+
 <a
-	href={href}
+	{href}
 	{...$$restProps}
-	class="relative font-medium text-blue-600 after:absolute after:bg-blue-600 after:-bottom-1 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 {$$restProps.class}"
+	class="relative font-medium text-blue-600 after:absolute after:bg-blue-600 after:-bottom-0.5 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 {$$restProps.class}"
 >
 	<slot />
 </a>
