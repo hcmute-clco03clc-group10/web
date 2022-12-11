c4f7c09a1df2bcefe1e900547cab4d8d2af2fc9d
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-11 23:31:07 +0700

    Add active prop.

--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	export let href: string;
+	export let active = false;
 </script>
 
 <a
 	{href}
 	{...$$restProps}
-	class="relative font-medium text-blue-600 after:absolute after:bg-blue-600 after:-bottom-0.5 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 {$$restProps.class}"
+	class="relative font-medium text-blue-600 after:absolute after:bg-blue-600 after:-bottom-0.5 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:transition-transform after:ease-in-out after:duration-300 {active
+		? 'after:origin-bottom-left after:scale-x-100'
+		: 'hover:after:origin-bottom-left hover:after:scale-x-100'} {$$restProps.class}"
 >
 	<slot />
 </a>
