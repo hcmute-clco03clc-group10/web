2c14922b5239d35f839cd2e0f8601f872a7ddd50
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-11 22:21:50 +0700

    Adjust disabled style.

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -6,13 +6,11 @@
 <button
 	on:click
 	{...$$restProps}
-	class={`group relative flex items-center rounded ${
-		!noPadding ? 'px-8 py-3' : ''
-	} transition duration-200 ease-in-out focus:outline-none focus:border-transparent border focus:ring focus:ring-blue-600/60 disabled:border-slate-500 disabled:text-slate-500 disabled:bg-slate-200 ` +
-		(solid ? 'bg-blue-600 text-slate-100 border-blue-700 ' : 'border-blue-600 text-blue-600 ') +
-		($$restProps.class || '')}
+	class="group relative flex items-center rounded transition duration-200 ease-in-out focus:outline-none focus:border-transparent border focus:ring focus:ring-blue-600/60 disabled:border-slate-400 disabled:text-slate-400 disabled:bg-slate-300
+	{!noPadding ? 'px-8 py-3' : ''}
+		{solid
+		? 'bg-blue-600 text-slate-100 border-blue-700 '
+		: 'border-blue-600 text-blue-600 '} {$$restProps.class || ''}"
 >
-	<span class="transition-transform duration-300 group-hover:duration-200 ease-in-out rotate-0 group-hover:rotate-180">
-		<slot />
-	</span>
+	<slot />
 </button>
