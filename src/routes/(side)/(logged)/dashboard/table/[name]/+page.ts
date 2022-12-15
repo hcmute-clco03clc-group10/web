ece8682d362050db3567c13f4756a071ab07da0a
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-12-16 05:04:50 +0700

    Conditionally fetch table items.

--- src/routes/(side)/(logged)/dashboard/table/[name]/+page.ts
+++ src/routes/(side)/(logged)/dashboard/table/[name]/+page.ts
@@ -1,7 +1,7 @@
 import { api } from '$lib/api';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch, params, depends }) => {
+export const load: PageLoad = async ({ fetch, params, depends, url }) => {
 	depends('dashboard/table/[name]');
 	return {
 		tableRef: new WeakRef(
@@ -16,15 +16,17 @@ export const load: PageLoad = async ({ fetch, params, depends }) => {
 				})
 		),
 		tableItemsRef: new WeakRef(
-			api
-				.use(fetch)
-				.get(`/table/${params.name}/item`)
-				.then((v) => {
-					if (v.status === 200) {
-						return v.json() as Promise<{ [key: string]: any }[]>;
-					}
-					return undefined;
-				})
+			!url.href.includes('#') || url.href.includes('#view-items')
+				? api
+						.use(fetch)
+						.get(`/table/${params.name}/item`)
+						.then((v) => {
+							if (v.status === 200) {
+								return v.json() as Promise<{ [key: string]: any }[]>;
+							}
+							return [];
+						})
+				: []
 		)
 	};
 };
